793a04a6f6f2d7ad8c05f4b6f8c35be52a8c7e2e
Taylor Sheets <taylor.sheets3@gmail.com>
2023-06-05 09:54:43 -0700

    Time: 80 ms (11.74%), Space: 42.9 MB (99.96%) - LeetHub

---
+++ 0083-remove-duplicates-from-sorted-list/0083-remove-duplicates-from-sorted-list.js
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+let deleteDuplicates = function(head) {
+    let node = head
+
+    while(node !== null){
+        if(node.next !== null){
+            if(node.val === node.next.val){
+                node.next = node.next.next
+                continue
+            }
+        }
+
+        node = node.next
+    }
+
+    return head
+};
