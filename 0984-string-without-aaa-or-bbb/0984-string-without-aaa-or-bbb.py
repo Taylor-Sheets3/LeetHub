d6c2edd29de66776d2bc499f9159eb0cd546020f
Taylor Sheets <taylor.sheets3@gmail.com>
2023-07-11 11:14:59 -0700

    Time: 41 ms (85.95%), Space: 16.1 MB (98.92%) - LeetHub

---
+++ 0984-string-without-aaa-or-bbb/0984-string-without-aaa-or-bbb.py
@@ -0,0 +1,26 @@
+class Solution:
+    def strWithout3a3b(self, a: int, b: int) -> str:
+
+        res = ''
+        while a or b:
+            if a and not b:
+                res += 'a' * a
+                a = 0
+            elif b and not a:
+                res += 'b' * b
+                b = 0
+            elif a > b:
+                res += 'aab'
+                a -=2
+                b -=1
+            elif b > a:
+                res += 'bba'
+                b-=2
+                a-=1
+            else:
+                res += 'ab' * a
+                a = 0
+                b = 0
+                
+            
+        return res
