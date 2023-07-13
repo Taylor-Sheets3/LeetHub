7556f72c593b1dee27eafa3ff0031acc8f4d68fc
Taylor Sheets <taylor.sheets3@gmail.com>
2023-07-13 07:23:13 -0700

    Time: 36 ms (95.72%), Space: 16.4 MB (23.70%) - LeetHub

--- 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.py
+++ 0693-binary-number-with-alternating-bits/0693-binary-number-with-alternating-bits.py
@@ -1,10 +1,5 @@
 class Solution:
     def hasAlternatingBits(self, n: int) -> bool:
-        binary = bin(n)[2:]
-        
-        if binary.count('11') or binary.count('00'):
-            return False
-        else:
-            return True
+        return not (bin(n)[2:].count('11') or bin(n)[2:].count('00'))
             
         
